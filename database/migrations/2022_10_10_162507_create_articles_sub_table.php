f1644a98bf0fc2519c15511dacd19d25eae2c834
白开水浇花 <fengqiyue@gmail.com>
2022-10-11 01:16:43 +0800

    分类、文章

---
+++ database/migrations/2022_10_10_162507_create_articles_sub_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        collect(range(0, 9))->each(function ($i) {
+            Schema::create('articles_'.$i, function (Blueprint $table) {
+                $table->unsignedBigInteger('id')->primary();
+                $table->string('source_url', 255)->nullable()->comment('来源链接');
+                $table->mediumText('content')->nullable()->comment('内容');
+                // 关联
+                $table->foreign('id')->references('id')->on('articles')->onDelete('cascade');
+            });
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        collect(range(0, 9))->each(function ($i) {
+            Schema::dropIfExists('articles_'.$i);
+        });
+    }
+};
