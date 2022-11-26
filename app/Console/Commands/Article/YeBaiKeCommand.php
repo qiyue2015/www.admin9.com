998658aa6351d5ff1ddee1a8bc5666d4a3b36ab1
白开水浇花 <fengqiyue@gmail.com>
2022-11-27 00:09:02 +0800

    采集脚本

--- app/Console/Commands/Article/YeBaiKeCommand.php
+++ app/Console/Commands/Article/YeBaiKeCommand.php
@@ -3,7 +3,11 @@
 namespace App\Console\Commands\Article;
 
 use App\Models\Article;
+use App\Models\Category;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Http;
+use Symfony\Component\DomCrawler\Crawler;
 
 class YeBaiKeCommand extends Command
 {
@@ -35,9 +39,12 @@ class YeBaiKeCommand extends Command
         if ($init === 1) {
             $this->init($num);
         }
+        if ($init === 2) {
+            $this->checkLink($num);
+        }
     }
 
-    protected function init($num)
+    protected function init($num): void
     {
         $star = 1;
         $maxId = (3502440 - 80); // 3502440
@@ -57,6 +64,72 @@ class YeBaiKeCommand extends Command
 
             $star += $num;
             $bar->advance($num);
+        }
+    }
+
+    protected function checkLink($num): void
+    {
+        $count = Article::where('checked', 0)->where('category_id', 1)->count();
+        $id = Article::where('checked', 0)->where('category_id', 1)->min('id');
+        $bar = $this->output->createProgressBar($count);
+
+        $i = 0;
+        while ($i < $count) {
+            $i++;
+            $bar->advance();
+
+            $list = Article::where('checked', 0)
+                ->where('id', '>', $id)
+                ->where('category_id', 1)
+                ->limit($num)
+                ->get();
+
+            if (empty($list)) {
+                dd('all ok.');
+            }
+
+            foreach ($list as $row) {
+                $id = $row->id;
+                $url = 'https://www.yebaike.com/e/action/ShowInfo.php?classid=32&id='.$id;
+                dispatch(static function () use ($url, $row) {
+                    $response = Http::withoutVerifying()->get($url);
+                    if (str()->contains($response->body(), '此信息不存在')) {
+                        $row->category_id++;
+                        $row->save();
+                    } else {
+                        try {
+                            $crawler = new Crawler();
+                            $crawler->addHtmlContent($response->body());
+                            $title = $crawler->filter('h1.title')->text();
+                            $date = $crawler->filterXPath('//div[@class="header"]/div/span[1]')->text();
+                            $categoryName = $crawler->filterXPath('//div[@class="header"]/div/span[2]/a')->text();
+                            $content = $crawler->filter('.article .text p')
+                                ->each(function (Crawler $cr) {
+                                    return '<p>'.$cr->html().'</p>';
+                                });
+
+                            $category = Category::firstOrCreate(['name' => $categoryName]);
+
+                            $row->category_id = $category->id;
+                            $row->title = $title;
+                            $row->checked = true;
+                            $row->created_at = now()->parse($date)->toDateTimeString();
+                            $row->save();
+
+                            $subtable = $row->id % 10;
+                            DB::table('articles_'.$subtable)->updateOrInsert([
+                                'id' => $row->id,
+                            ], [
+                                'content' => implode(PHP_EOL, $content),
+                            ]);
+
+                        } catch (\Exception $e) {
+                            \Log::debug($e->getMessage());
+                        }
+                    }
+                });
+            }
+
         }
     }
 }
