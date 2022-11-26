7cfe510a585b210eb18a145360d2ca449d844a74
白开水浇花 <fengqiyue@gmail.com>
2022-11-26 22:04:13 +0800

    增加部署脚本.

---
+++ deploy.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+DEPLOY_OPTION=${1:-all}
+MASTER_PATH=/www/wwwroot/www.admin9.com
+
+redEcho(){
+   echo -e "[ $1 ]"
+}
+
+deployProd(){
+  source /etc/profile
+
+  redEcho "开始部署"
+  echo $(date "+%Y-%m-%d %H:%M:%S")
+
+  cd ${MASTER_PATH} || exit
+  redEcho '进入网站目录'
+  pwd
+
+  redEcho 'git reset & clean'
+  git reset --hard
+  git clean -df
+
+  redEcho '拉代码 git pull'
+  git pull
+  redEcho 'git 最近一次日志'
+  git log --pretty=format:"%h %cd %cr %s (%cn)" -1  | xargs -0 echo
+
+  if [ "$1" != 'frontend' ]; then
+#       redEcho '清理 opcache'
+#       /usr/local/bin/php artisan deploy:opcache clear
+
+       redEcho '安装 composer 依赖'
+       /usr/local/bin/composer install --optimize-autoloader --no-dev
+
+       redEcho '重置 route & config 缓存'
+       /usr/local/bin/php artisan optimize
+
+       redEcho '执行数据库迁移'
+       /usr/local/bin/php artisan migrate --force
+
+#       redEcho '重启队列'
+#       /usr/local/bin/php artisan horizon:terminate
+  fi
+
+  #  if [ "$1" != 'backend'  ]; then
+  #       redEcho '进入前端目录'
+  #       cd frontend || exit
+  #       pwd
+  #
+  #       redEcho '前端构建'
+  #       yarn install
+  #       yarn run build:prod
+  #  fi
+
+  redEcho '完成'
+}
+
+
+if [[ -d ${MASTER_PATH} ]]; then
+  deployProd "${DEPLOY_OPTION}"
+fi
