db23c87464c4c3cf3fa223400ad2b56d25218865
白开水浇花 <fengqiyue@gmail.com>
2022-11-26 22:26:46 +0800

    增加频道和迁移文件

---
+++ app/Models/Channel.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * App\Models\Channel
+ *
+ * @property int $id
+ * @property string $name 名称
+ * @property int $sort 排序
+ * @method static \Illuminate\Database\Eloquent\Builder|Channel newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Channel newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Channel query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Channel whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Channel whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Channel whereSort($value)
+ * @mixin \Eloquent
+ */
+class Channel extends Model
+{
+    use HasFactory;
+
+    public $timestamps = false;
+
+    protected $guarded = [];
+}
---
+++ database/migrations/2022_11_04_020943_create_channels_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('channels', function (Blueprint $table) {
+            $table->smallIncrements('id');
+            $table->string('name', 20)->unique()->comment('名称');
+            $table->smallInteger('sort')->default(0)->comment('排序');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('channels');
+    }
+};
