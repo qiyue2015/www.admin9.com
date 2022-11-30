1b36ff8ce15e7670ab633c5537415ae2e2374f29
白开水浇花 <fengqiyue@gmail.com>
2022-11-30 21:33:48 +0800

    调整为每分钟执行200

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -18,7 +18,7 @@ class Kernel extends ConsoleKernel
         // $schedule->command('inspire')->hourly();
 
         // 每 5 分钟运行一次采集
-        $schedule->command('article:yebaike --init=2 --num=1000')->everyFiveMinutes()->withoutOverlapping();
+        $schedule->command('article:yebaike --init=2 --num=200')->everyMinute()->withoutOverlapping();
     }
 
     /**
