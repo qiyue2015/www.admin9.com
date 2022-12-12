394123deb16afb5cd500f1d2a399f7992deb918c
白开水浇花 <fengqiyue@gmail.com>
2022-12-12 21:40:57 +0800

    索引调整.

--- database/migrations/2022_12_10_183459_add_field_index_to_articles_table.php
+++ database/migrations/2022_12_10_183459_add_field_index_to_articles_table.php
@@ -13,7 +13,7 @@ return new class extends Migration {
     public function up()
     {
         Schema::table('articles', function (Blueprint $table) {
-            $table->index(['category_id', 'checked']);
+            $table->index(['id', 'category_id', 'checked']);
         });
     }
 
@@ -25,7 +25,7 @@ return new class extends Migration {
     public function down()
     {
         Schema::table('articles', function (Blueprint $table) {
-            //
+            $table->dropIndex('articles_category_id_checked_index');
         });
     }
 };
