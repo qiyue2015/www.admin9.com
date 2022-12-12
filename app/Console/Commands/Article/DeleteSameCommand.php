4a193d42b3218a45385faea70ffa2882900793f7
白开水浇花 <fengqiyue@gmail.com>
2022-12-12 19:06:30 +0800

    test1

--- app/Console/Commands/Article/DeleteSameCommand.php
+++ app/Console/Commands/Article/DeleteSameCommand.php
@@ -33,21 +33,26 @@ class DeleteSameCommand extends Command
         $this->comment('删除文章表重复标题…');
 
         $star = 1;
-        $num = 0;
+        $limit = 10;
+        $page = 0;
         while ($star) {
             $this->info('正在执行（'.$star.'）');
 
+            $skip = $page * $limit;
             $list = Article::select(DB::raw('COUNT(title) as dd, title'))
-                ->where('id', '>=', $star)
                 ->groupBy('title')
                 ->orderByDesc('dd')
-                ->take(10)
+                ->skip($skip)
+                ->take($limit)
                 ->get();
+            dd($list);
 
             if ($list->isEmpty()) {
                 $this->info('全部执行完毕.');
                 break;
             }
+
+            $page++;
 
             foreach ($list as $row) {
                 $star = $row->id;
@@ -57,19 +62,17 @@ class DeleteSameCommand extends Command
                     break 2;
                 }
 
-                $num++;
-
-                dispatch(static function () use ($title) {
-                    Article::whereTitle($title)->get(['id', 'title'])->each(function ($row, $index) {
-                        if ($index) {
-                            $row->delete();
-                            Log::channel('same')->info('删除重复标题', [
-                                'id' => $row->id,
-                                'title' => $row->title,
-                            ]);
-                        }
-                    });
-                })->onQueue('default');
+                //dispatch(static function () use ($title) {
+                //    Article::whereTitle($title)->get(['id', 'title'])->each(function ($row, $index) {
+                //        if ($index) {
+                //            $row->delete();
+                //            Log::channel('same')->info('删除重复标题', [
+                //                'id' => $row->id,
+                //                'title' => $row->title,
+                //            ]);
+                //        }
+                //    });
+                //})->onQueue('default');
             }
         }
 
