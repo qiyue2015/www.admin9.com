89d9883d9185a19820ec89d8028d0f7f572ff9a7
白开水浇花 <fengqiyue@gmail.com>
2022-12-12 22:26:52 +0800

    fix: 上一页下一页不准确.

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -66,6 +66,7 @@ class ArticleController extends Controller
             return Article::where('id', '<', $article->id)
                 //->where('category_id', $article->category_id)
                 ->checked()
+                ->orderByDesc('id')
                 ->first();
         });
 
@@ -73,6 +74,7 @@ class ArticleController extends Controller
             return Article::where('id', '>', $article->id)
                 //->where('category_id', $article->category_id)
                 ->checked()
+                ->orderByDesc('id')
                 ->first();
         });
 
