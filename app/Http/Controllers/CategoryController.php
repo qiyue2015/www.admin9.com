eb8bd64727cb077380526d2de4a229b807d64657
白开水浇花 <fengqiyue@gmail.com>
2022-12-21 23:12:23 +0800

    完成分类接口.

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\CategoryAddRequest;
+use App\Models\Category;
+use Illuminate\Http\JsonResponse;
+
+class CategoryController extends Controller
+{
+    /**
+     * 分类例表
+     *
+     * @return JsonResponse
+     */
+    public function index(): JsonResponse
+    {
+        $list = Category::orderBy('sort')->get();
+        return $this->success($list);
+    }
+
+    /**
+     * 添加栏目
+     *
+     * @param  CategoryAddRequest  $request
+     * @return JsonResponse
+     */
+    public function store(CategoryAddRequest $request): JsonResponse
+    {
+        $params = $request->all();
+        $params['children'] = [];
+        $params['parents'] = [];
+        if ($params['parent_id']) {
+            $parent = Category::findOrFail($params['parent_id']);
+            if ($parent->is_last) {
+                return $this->fail('父栏目不能是终极栏目');
+            }
+            $params['parents'] = [...$parent->parents, ...[$parent->id]];
+        }
+
+        $category = Category::create($params);
+
+        if ($category->parent_id) {
+            // 修改父栏目的子栏目
+            $parent = Category::findOrFail($category->parent_id);
+            $parent->children = [...$parent->children, ...[$category->id]];
+            $parent->save();
+
+            // 更改父类别的父栏目的子栏目
+            Category::whereIn('id', $parent->parents)->each(function ($super) use ($category) {
+                $super->children = [...$super->children, ...[$category->id]];
+                $super->save();
+            });
+        }
+
+        return $this->success($category);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Category $category)
+    {
+        //
+    }
+
+    /**
+     * 修改分类
+     *
+     * @param  CategoryAddRequest  $request
+     * @param  Category  $category
+     * @return JsonResponse
+     */
+    public function update(CategoryAddRequest $request, Category $category): JsonResponse
+    {
+        $params = $request->all();
+        if ($params['parent_id'] && $params['parent_id'] !== $category->parent_id) {
+            // 大栏目跟原栏目相同
+            if ($params['parent_id'] === $category->id) {
+                return $this->fail('您选择的大栏目跟本栏目是同一对象，请重新选择大栏目');
+            }
+
+            // 取得现在大栏目
+            $parent = Category::findOrFail($params['parent_id']);
+            if ($parent->is_last) {
+                return $this->fail('父栏目不能是终极栏目');
+            }
+
+            // 是否非法父栏目
+            if (!$params['is_last'] && $parent->children && in_array($category->id, $parent->children, true)) {
+                return $this->fail('您选择隶属的大栏目是栏目本身的子栏目。请重新选择大栏目');
+            }
+        }
+
+        $category->update($params);
+        if ($category->parent_id) {
+            $categories = Category::all(['id', 'parent_id'])->toArray();
+
+            // 修改父栏目的子栏目
+            $category->children = getChildCategories($category->id, $categories);
+
+            // 更改父类别的父栏目的子栏目
+            $parents = getParentCategories($category->id, $categories);
+            Category::whereIn('id', $parents)->each(function ($super) use ($categories) {
+                $super->children = getChildCategories($super->id, $categories);
+                $super->save();
+            });
+        }
+
+        return $this->success();
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Category $category)
+    {
+        if (!$category->is_last) {
+            // 删除子栏目
+            Category::whereIn('id', $category->children)->delete();
+        }
+
+        // 删除栏目本身
+        $category->delete();
+
+        return $this->success();
+    }
+}
---
+++ database/migrations/2022_12_21_012625_create_categories_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->smallIncrements('id');
+            $table->unsignedSmallInteger('parent_id')->index()->default(0)->comment('父ID');
+            $table->string('name', 20)->comment('名称');
+            $table->smallInteger('sort')->default(0)->comment('排序');
+            $table->unsignedTinyInteger('is_last')->default(0)->comment('0非终极栏目 1终极栏目');
+            $table->unsignedTinyInteger('is_list')->default(0)->comment('0封面模式 1列表模式');
+            $table->text('children')->comment('子栏目ID集合');
+            $table->text('parents')->comment('父栏目ID集合');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+};
