72f7063b9bbe759b80b7f2978c89941ab0c4c505
白开水浇花 <fengqiyue@gmail.com>
2022-12-28 13:07:28 +0800

    通过自定义模型获取分类

---
+++ app/Console/Commands/Init/InitCategoryIdCommand.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Console\Commands\Init;
+
+use App\Jobs\InitCategoryIdJob;
+use App\Models\Article;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Http;
+
+class InitCategoryIdCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'init:category-id';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = '通过百度自定义标签模型对内容进行分类';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        Article::whereCategoryId(99)
+            ->orderByDesc('id')
+            ->limit(1000)->each(function ($article) {
+                InitCategoryIdJob::dispatch($article);
+            });
+    }
+}
---
+++ app/Jobs/InitCategoryIdJob.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Article;
+use Exception;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Http;
+
+class InitCategoryIdJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected string $key = 'jcTPUIkefcCLgxQbF5DVz9By';
+
+    protected string $secret = '6Kdm7fbNEOAeGqOzqCBV2UuEtbyLLjna';
+
+    private Article $article;
+
+    private function getToken()
+    {
+        return Cache::remember('baidu-ai-token', now()->addDays(30), function () {
+            $url = 'https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id='.$this->key.'&client_secret='.$this->secret;
+            return Http::get($url)->json('access_token');
+        });
+    }
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(Article $article)
+    {
+        $this->article = $article;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $subtable = 'articles_'.($this->article->id % 10);
+        $item = DB::table($subtable)->find($this->article->id);
+        if ($item) {
+            try {
+                $url = 'https://aip.baidubce.com/rpc/2.0/ai_custom/v1/text_cls/baike?access_token='.$this->getToken();
+                $response = Http::post($url, [
+                    'text' => $this->article->title.PHP_EOL.strip_tags($item->content),
+                    'threshold' => 0.6,
+                ]);
+                $results = $response->json('results');
+                $tags = collect($results)->sortByDesc('score')->map(function ($row) {
+                    return $row['name'];
+                });
+                DB::table($subtable)->where('id', $this->article->id)->update([
+                    'tags' => implode(',', $tags->toArray()),
+                ]);
+                $this->article->update(['category_id' => 1]);
+            } catch (Exception $exception) {
+                $this->fail($exception);
+            }
+        }
+    }
+}
