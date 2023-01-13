4d3d7b39a9aebd18dd20d9f2ec4e5ca03a6ede3a
白开水浇花 <fengqiyue@gmail.com>
2023-01-13 15:18:48 +0800

    test ['mode' => 'boolean']

--- app/Console/Commands/Test.php
+++ app/Console/Commands/Test.php
@@ -30,7 +30,7 @@ class Test extends Command
     public function handle(): void
     {
         $keyword = $this->argument('keyword');
-        $list = Article::whereFullText('title', $keyword)->take(10);
+        $list = Article::whereFullText('title', $keyword, ['mode' => 'boolean'])->take(10);
         dd($list->get(['id', 'title'])->toArray());
         //$path = '/Users/fengqiyue/Documents/Project/caishengfeiyang/tv/videos';
         //$files = \File::files($path);
