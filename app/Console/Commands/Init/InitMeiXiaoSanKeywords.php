71840b6372c99521afabfe3f4d2f2ef289df0237
白开水浇花 <fengqiyue@gmail.com>
2023-02-05 21:53:01 +0800

    提取关键词.

---
+++ app/Console/Commands/Init/InitMeiXiaoSanKeywords.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Console\Commands\Init;
+
+use App\Jobs\Init\InitMeiXiaoSanKeywordsJob;
+use App\Models\Article;
+use Illuminate\Console\Command;
+use Illuminate\Database\Eloquent\Builder;
+
+class InitMeiXiaoSanKeywords extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'init:meixiaosan-keywords';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     */
+    public function handle(): void
+    {
+        $count = $this->query()->count();
+        $lastId = $this->query()->max('id');
+        $bar = $this->output->createProgressBar($count);
+        $star = 0;
+        while ($star < $lastId) {
+            $list = $this->query()->where('id', '>', $star)->take(10)->get(['id', 'title']);
+            $star = $list->last()->id;
+            $bar->advance($list->count());
+            collect($list)->each(function ($article) {
+                InitMeiXiaoSanKeywordsJob::dispatch($article)->onQueue('just_for_article');
+            });
+        }
+    }
+
+    private function query(): Article|Builder
+    {
+        return Article::checked()->where('status', 0);
+    }
+}
