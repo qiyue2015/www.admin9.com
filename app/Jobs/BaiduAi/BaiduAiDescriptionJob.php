40e80207b25e2240aa1626f325061cc3140b3e77
白开水浇花 <fengqiyue@gmail.com>
2023-02-12 09:26:54 +0800

    fix

--- app/Jobs/BaiduAi/BaiduAiDescriptionJob.php
+++ app/Jobs/BaiduAi/BaiduAiDescriptionJob.php
@@ -44,7 +44,7 @@ class BaiduAiDescriptionJob implements ShouldQueue
                 $url = 'https://aip.baidubce.com/rpc/2.0/nlp/v1/news_summary?charset=UTF-8&access_token='.$this->getToken();
                 $data = [
                     'title' => Str::limit($this->article->title, 40, ''),
-                    'content' => Str::limit($this->content, 6000),
+                    'content' => Str::limit($this->content, 4000),
                     'max_summary_len' => 150,
                 ];
                 $response = Http::asJson()->post($url, $data);
