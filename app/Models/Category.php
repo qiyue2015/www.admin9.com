24b0f9aaa0967f107fa1e3e2df005f0edb7b5273
白开水浇花 <fengqiyue@gmail.com>
2023-02-16 23:18:24 +0800

    清理注释.

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -12,9 +12,6 @@ use Illuminate\Database\Eloquent\Model;
  * @property int $parent_id 父ID
  * @property string $name 名称
  * @property int $sort 排序
- * @property int $num 分类信息数量
- * @property int $is_show 导航展示 1展示
- * @property int $baike_classid 对应百科分类
  * @property int $is_last 0非终极栏目 1终极栏目
  * @property int $is_list 0封面模式 1列表模式
  * @property array $children 子栏目ID集合
@@ -22,14 +19,11 @@ use Illuminate\Database\Eloquent\Model;
  * @method static \Illuminate\Database\Eloquent\Builder|Category newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Category newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Category query()
- * @method static \Illuminate\Database\Eloquent\Builder|Category whereBaikeClassid($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Category whereChildren($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Category whereId($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Category whereIsLast($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Category whereIsList($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Category whereIsShow($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Category whereName($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Category whereNum($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Category whereParentId($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Category whereParents($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Category whereSort($value)
--- app/Models/Dtime.php
+++ app/Models/Dtime.php
@@ -8,20 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 /**
  * App\Models\Dtime
  *
- * @property int $id
- * @property string $title
- * @property string $url
- * @property string $url_hash
  * @method static \Illuminate\Database\Eloquent\Builder|Dtime newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Dtime newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Dtime query()
- * @method static \Illuminate\Database\Eloquent\Builder|Dtime whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Dtime whereTitle($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Dtime whereUrl($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Dtime whereUrlHash($value)
  * @mixin \Eloquent
- * @property int $status
- * @method static \Illuminate\Database\Eloquent\Builder|Dtime whereStatus($value)
  */
 class Dtime extends Model
 {
