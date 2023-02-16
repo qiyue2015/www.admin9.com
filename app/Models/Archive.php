5d775e59fc11fc49c13b16154b56b48bb819659e
白开水浇花 <fengqiyue@gmail.com>
2023-02-16 23:08:52 +0800

    增加TAGS扩展包.

--- app/Models/Archive.php
+++ app/Models/Archive.php
@@ -4,8 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Conner\Tagging\Taggable;
 
 class Archive extends Model
 {
-    use HasFactory;
+    use HasFactory, Taggable;
 }
