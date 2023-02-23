e19ad52ebb75b448710f3801a773460d68b1091a
白开水浇花 <fengqiyue@gmail.com>
2023-02-23 08:55:02 +0800

    移除 tagging

--- app/Models/Archive.php
+++ app/Models/Archive.php
@@ -2,11 +2,8 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Conner\Tagging\Taggable;
 
 class Archive extends Model
 {
-    use HasFactory, Taggable;
 }
