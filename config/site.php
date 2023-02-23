6c35535e9ab7543c43b3e83fffc4791d40186617
白开水浇花 <fengqiyue@gmail.com>
2023-02-23 18:48:25 +0800

    site config

--- config/site.php
+++ config/site.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'sitename' => '好奇网 haoqi.net',
-    'keywords' => '好奇网,好奇网,好奇问答,好奇星球,haoqi.net',
-    'description' => '用好奇心驱动世界，一起探索未知的力量。',
-    'email' => 'moc.liamg@iqoah', //    haoqi@gmail.com 邮箱反写前端会转为转为正常顺序
+    'sitename' => env('SITE_NAME', '好奇网 haoqi.net'),
+    'keywords' => env('SITE_KEYWORDS', '好奇网,好奇网,好奇问答,好奇星球,haoqi.net'),
+    'description' => env('SITE_DESCRIPTION', '用好奇心驱动世界，一起探索未知的力量。'),
+    'email' => env('SITE_EMAIL', 'moc.liamg@iqoah'), //    haoqi@gmail.com 邮箱反写前端会转为转为正常顺序
 ];
