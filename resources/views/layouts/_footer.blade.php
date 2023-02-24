22301340278a597f3ee8330e11d405c9cd9f68bc
白开水浇花 <fengqiyue@gmail.com>
2023-02-24 15:02:16 +0800

    添加公安备案

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,6 +1,10 @@
 <footer class="footer footer-center">
     <div>
-        <p>Copyright © 2023 - All right reserved by {{ config('site.name') }} Ltd <a href="http://beian.miit.gov.cn/" rel="nofollow noreferrer" target="_blank">蜀ICP备2022018286号-2</a></p>
+        <p>
+            Copyright © 2023 - All right reserved by {{ config('site.name') }} Ltd
+            <a href="http://beian.miit.gov.cn/" rel="nofollow noreferrer" target="_blank">{{ config('site.beian') }}</a>
+            <a href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=11010802040184" rel="nofollow noreferrer" target="_blank">{{ config('site.gongan_beian') }}</a>
+        </p>
         <p>侵权举报：本页面所涉内容为用户发表并上传，相应的法律责任由用户自行承担，本网站仅提供存储服务；如存在侵权问题，请权利人与本网站联系删除！举报邮箱: <span class="email-link">{{ config('site.email') }}</span></p>
     </div>
 </footer>
