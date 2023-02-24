7487dfae0db89d9e7a31656dbad56e97d59c2953
白开水浇花 <fengqiyue@gmail.com>
2023-02-24 16:31:18 +0800

    增加显示数量

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,7 +10,7 @@
 @endif
 @section('content')
     <div class="article-list">
-@loop($category_id, 50)
+@loop($category_id, 300)
         <div class="article-item">
             <a href="{{ $row->link() }}" class="article-img">
                 <img src="{{ $row->cover }}" alt="{{ $row->title }}" class="object-cover rounded"/>
