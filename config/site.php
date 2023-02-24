a660eb59c779c70ae0fafcda064743e923ba7d52
白开水浇花 <fengqiyue@gmail.com>
2023-02-24 09:51:45 +0800

    增加备案号.

--- config/site.php
+++ config/site.php
@@ -4,5 +4,6 @@ return [
     'name' => env('SITE_NAME', '好奇网 haoqi.net'),
     'keywords' => env('SITE_KEYWORDS', '好奇网,好奇网,好奇问答,好奇星球,haoqi.net'),
     'description' => env('SITE_DESCRIPTION', '用好奇心驱动世界，一起探索未知的力量。'),
+    'beian' => env('SITE_BEIAN', ''),
     'email' => env('SITE_EMAIL', 'moc.liamg@iqoah'), //    haoqi@gmail.com 邮箱反写前端会转为转为正常顺序
 ];
