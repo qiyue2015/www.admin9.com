7ffbabdbfcf5a66c572e1ff2928f44b8bf0ae381
白开水浇花 <fengqiyue@gmail.com>
2023-02-27 07:12:54 +0800

    _ide_helper_models

--- app/Models/Archive.php
+++ app/Models/Archive.php
@@ -19,26 +19,34 @@ use Jenssegers\Optimus\Optimus;
  * @property string|null $search_title
  * @property string|null $keywords
  * @property string|null $description
- * @property string|null $tags
- * @property int $has_cover 是否有封面
  * @property string $cover
+ * @property array $tags
+ * @property string|null $writer 作者
+ * @property string|null $source_name 来源名称
+ * @property string|null $source_url 来源地址
+ * @property int $has_cover 是否有封面
  * @property string|null $images
  * @property int $view_num 点击量
+ * @property int $user_id 后台发布ID
+ * @property int $checked
  * @property int $is_publish
- * @property string|null $publish_at
+ * @property string $publish_at
  * @property int $is_html 生成 PC html
  * @property int $is_wap_html 生成 WAP html
  * @property int $is_sitemap 生成
- * @property string $baidu_id
+ * @property string|null $task_id
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
  * @property-read \App\Models\Category|null $category
  * @property-read \App\Models\ArchiveExtend|null $extend
+ * @property-read \App\Models\User|null $user
+ * @method static \Illuminate\Database\Eloquent\Builder|Archive checked()
  * @method static \Illuminate\Database\Eloquent\Builder|Archive newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Archive newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Archive published()
  * @method static \Illuminate\Database\Eloquent\Builder|Archive query()
- * @method static \Illuminate\Database\Eloquent\Builder|Archive whereBaiduId($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Archive whereCategoryId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Archive whereChecked($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Archive whereCover($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Archive whereCreatedAt($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Archive whereDescription($value)
@@ -53,17 +61,16 @@ use Jenssegers\Optimus\Optimus;
  * @method static \Illuminate\Database\Eloquent\Builder|Archive whereKeywords($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Archive wherePublishAt($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Archive whereSearchTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Archive whereSourceName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Archive whereSourceUrl($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Archive whereSubtitle($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Archive whereTags($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Archive whereTaskId($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Archive whereTitle($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Archive whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Archive whereUserId($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Archive whereViewNum($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Archive published()
- * @property int $checked
- * @property string|null $task_id
- * @method static \Illuminate\Database\Eloquent\Builder|Archive checked()
- * @method static \Illuminate\Database\Eloquent\Builder|Archive whereChecked($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Archive whereTaskId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Archive whereWriter($value)
  * @mixin \Eloquent
  */
 class Archive extends Model
