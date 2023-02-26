c3256945b087351a004947f26a5a5b2d9d90097c
白开水浇花 <fengqiyue@gmail.com>
2023-02-27 07:23:44 +0800

    电视节目制作许可证链接修改.

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -5,7 +5,7 @@
             <a href="http://beian.miit.gov.cn/" rel="nofollow noreferrer" class="ml-2" target="_blank">{{ config('site.beian') }}</a>
             <a href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=11010802040184" rel="nofollow noreferrer" target="_blank">{{ config('site.gongan_beian') }}</a>
         </p>
-        <p>广播电视节目制作经营许可证：<a href="http://gdj.beijing.gov.cn/xzxkxxgsNEW/2022/202206/t20220607_2731639.html" rel="nofollow noreferrer" target="_blank">(京)字第13910号</a> <span class="ml-2">国内互联网虚拟专用网业务B1-20196045</span></p>
+        <p>广播电视节目制作经营许可证：<a href="http://gdj.beijing.gov.cn/" rel="nofollow noreferrer" target="_blank">(京)字第13910号</a> <span class="ml-2">国内互联网虚拟专用网业务B1-20196045</span></p>
         <p>侵权举报：本页面所涉内容为用户发表并上传，相应的法律责任由用户自行承担，本网站仅提供存储服务；如存在侵权问题，请权利人与本网站联系删除！邮箱: <span class="email-link">{{ config('site.email') }}</span></p>
     </div>
 </footer>
