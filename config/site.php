d1534c878bf4517b0746edacde0bdb153ef8dc32
白开水浇花 <fengqiyue@gmail.com>
2023-02-27 08:02:51 +0800

    底部信息从配置获得.

--- config/site.php
+++ config/site.php
@@ -4,7 +4,9 @@ return [
     'name' => env('SITE_NAME', 'laravel'),
     'keywords' => env('SITE_KEYWORDS', ''),
     'description' => env('SITE_DESCRIPTION', ''),
+    'email' => env('SITE_EMAIL', ''),
     'beian' => env('SITE_BEIAN', ''),
     'gongan_beian' => env('SITE_GONGAN_BEIAN', ''),
-    'email' => env('SITE_EMAIL', ''),
+    'gdj_beian' => env('SITE_GDJ_BEIAN', ''),
+    'ip_vpn_beian' => env('SITE_IP_VPN_BEIAN', ''),
 ];
