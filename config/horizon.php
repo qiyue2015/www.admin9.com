6ed3a31df7a5fb6df676ecf101b2882f5ec2e0d6
白开水浇花 <fengqiyue@gmail.com>
2023-02-28 10:49:51 +0800

    加大剂量。

--- config/horizon.php
+++ config/horizon.php
@@ -241,7 +241,7 @@ return [
             'just_for_toutiao_wenba' => [
                 'connection' => 'redis',
                 'queue' => [CustomQueue::SPIDER_TOUTIAO_WENBA_QUEUE],
-                'processes' => 10,
+                'processes' => 20,
                 'tries' => 1,
                 'timeout' => 30,
             ],
