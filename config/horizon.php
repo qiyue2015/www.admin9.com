7aaac021537acc8b2b76db78f8eb3bd6d6ee0fc2
白开水浇花 <fengqiyue@gmail.com>
2023-03-03 13:13:04 +0800

    貌似队列还可以再来一点.

--- config/horizon.php
+++ config/horizon.php
@@ -241,7 +241,7 @@ return [
             'just_for_toutiao_wenba' => [
                 'connection' => 'redis',
                 'queue' => [CustomQueue::SPIDER_TOUTIAO_WENBA_QUEUE],
-                'processes' => 20,
+                'processes' => 30,
                 'tries' => 1,
                 'timeout' => 30,
             ],
