b6f5819d89e6cdf20e56a1724bc396586b85b7bb
白开水浇花 <fengqiyue@gmail.com>
2023-03-06 02:01:38 +0800

    返回真实ID

--- app/Console/Commands/Test.php
+++ app/Console/Commands/Test.php
@@ -12,6 +12,7 @@ use Illuminate\Console\Command;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
+use Jenssegers\Optimus\Optimus;
 use Overtrue\Pinyin\Pinyin;
 
 class Test extends Command
@@ -21,7 +22,7 @@ class Test extends Command
      *
      * @var string
      */
-    protected $signature = 'test';
+    protected $signature = 'test {key}';
 
     /**
      * The console command description.
@@ -38,42 +39,8 @@ class Test extends Command
      */
     public function handle()
     {
-        $lastId = Archive::where('has_cover', 0)->max('id');
-        $count = Archive::where('has_cover', 0)->count();
-        $bar = $this->output->createProgressBar($count);
-        $star = 0;
-        while ($star < $lastId) {
-            $list = Archive::where('has_cover', 0)->where('id', '>', $star)->limit(200)->get(['id', 'title', 'tags']);
-            if ($list->isEmpty()) {
-                break;
-            }
-
-            $bar->advance($list->count());
-            $star = $list->last()->id;
-
-            foreach ($list as $row) {
-                dispatch(static function () use ($row) {
-                    $cover = makeTitleCover($row->title, true);
-                    $row->update([
-                        'cover' => $cover ?: null,
-                        'has_cover' => (bool) $cover,
-                    ]);
-                })->onQueue(CustomQueue::LARGE_PROCESSES_QUEUE);
-            }
-        }
-    }
-
-    private function getCategory($string): Model|Category
-    {
-        $arr = explode(',', $string);
-        return Category::firstOrCreate([
-            'alias' => $arr[0],
-            'parent_id' => 0,
-        ], [
-            'name' => $arr[0],
-            'slug' => Pinyin::permalink($arr[0], ''),
-            'children' => [],
-            'parents' => [],
-        ]);
+        $id = $this->argument('key');
+        $encode_id = app(Optimus::class)->decode($id);
+        $this->info($encode_id);
     }
 }
