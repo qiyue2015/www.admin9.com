26e9eb686f7cac0294dcb983f950b6567f303e41
白开水浇花 <fengqiyue@gmail.com>
2023-03-05 22:36:43 +0800

    迁移字段对到服务器上的进行调整.

--- database/migrations/2023_02_24_001520_create_archives_table.php
+++ database/migrations/2023_02_24_001520_create_archives_table.php
@@ -13,18 +13,21 @@ return new class extends Migration {
     public function up()
     {
         Schema::create('archives', function (Blueprint $table) {
-            $table->comment('');
+            $table->comment('文档表');
             $table->bigIncrements('id');
-            $table->string('title');
             $table->unsignedSmallInteger('category_id')->default(0)->comment('分类ID');
-            $table->set('flag', ['c', 'h', 'p', 'f', 's', 'j', 'a', 'b'])->nullable();
-            $table->char('subtitle', 100)->nullable();
+            $table->string('title');
+            $table->char('subtitle', 36)->nullable();
             $table->char('search_title', 100)->nullable();
             $table->char('keywords', 50)->nullable();
             $table->string('description')->nullable();
-            $table->string('cover')->nullable();
-            $table->string('tags')->nullable();
+            $table->set('flag', ['c', 'h', 'p', 'f', 's', 'j', 'a', 'b'])->nullable();
             $table->boolean('has_cover')->default(false)->index()->comment('是否有封面');
+            $table->char('cover', 100)->nullable()->comment('封面图片');
+            $table->string('tags')->nullable();
+            $table->char('writer', 20)->comment('作者');
+            $table->string('source_name', 20)->comment('来源');
+            $table->string('source_url')->comment('来源URL');
             $table->text('images')->nullable();
             $table->unsignedBigInteger('view_num')->default(0)->comment('点击量');
             $table->boolean('checked')->default(false)->index();
@@ -32,7 +35,7 @@ return new class extends Migration {
             $table->timestamp('publish_at')->nullable();
             $table->boolean('is_html')->default(false)->index()->comment('生成 PC html');
             $table->boolean('is_wap_html')->default(false)->index()->comment('生成 WAP html');
-            $table->boolean('is_sitemap')->default(false)->index()->comment('生成');
+            $table->boolean('is_sitemap')->default(false)->index()->comment('生成 地图 html');
             $table->char('task_id', 32)->nullable()->index();
             $table->timestamps();
 
