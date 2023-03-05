dcf5dca37df7f823bcc6e60e0e979235c4fe7f29
白开水浇花 <fengqiyue@gmail.com>
2023-03-05 16:02:32 +0800

    清理代码，与 chat GPT 完成生成图片功能

---
+++ app/Http/Controllers/Api/CommonController.php
@@ -0,0 +1,81 @@
+<?php
+
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Storage;
+use Intervention\Image\Facades\Image;
+use Illuminate\Support\Facades\Validator;
+
+class CommonController extends Controller
+{
+    public function generateTitleImage(Request $request): JsonResponse
+    {
+        /**
+         * 验证请求参数
+         */
+        $validator = Validator::make($request->all(), [
+            'title' => 'required|min:5|max:30',
+        ]);
+
+        if ($validator->fails()) {
+            return $this->fail($validator->errors()->first());
+        }
+
+        $title = $request->input('title');
+
+        // 底图目录
+        $backgroundDir = resource_path('title_backgrounds');
+        $backgrounds = File::files($backgroundDir);
+        if (empty($backgrounds)) {
+            return $this->fail('背景图片为空.');
+        }
+
+        // 随机获取一张背景图
+        $background = $backgrounds[array_rand($backgrounds)];
+
+        // 字体
+        $fontSize = getFontSize($title);
+        $fontPath = resource_path('fonts/zcool-yangyu-W04.ttf');
+
+        // 根据文件名里的关键字来决定文字使用颜色
+        $color = '#000000';
+        if (str()->contains($background->getBasename(), 'white')) {
+            $color = '#FFFFFF';
+        } elseif (str()->contains($background->getBasename(), 'tea')) {
+            $color = '#134e4a';
+        }
+
+        // 判断图片是否已存在
+        $imageFilename = md5($title).'.'.$background->getExtension();
+        $imagePath = 'title_background_temps/'.$imageFilename;
+        if (Storage::disk('public')->exists($imagePath)) {
+            return $this->success([
+                'image_url' => Storage::url($imagePath),
+            ]);
+        }
+
+        // 生成图片
+        $imageTempPath = Storage::disk('public')->path($imagePath);
+        Image::make($background->getPathname())
+            ->text($title, 400, 160, function ($font) use ($fontSize, $fontPath, $color) {
+                $font->file($fontPath);
+                $font->size($fontSize);
+                $font->color($color);
+                $font->align('center');
+                $font->valign('center');
+            })
+            ->save($imageTempPath);
+
+        return $this->success([
+            'image_url' => Storage::url($imagePath),
+        ]);
+    }
+
+}
+
--- routes/api/common.php
+++ routes/api/common.php
@@ -1,8 +1,14 @@
 <?php
 
+use App\Http\Controllers\Api\CommonController;
 use Illuminate\Http\Request;
 
 Route::post('webhook', function (Request $request) {
     \Illuminate\Support\Facades\Log::info('Chat GPT', ['content' => $request->post()]);
     return '操作成功';
 });
+
+Route::controller(CommonController::class)->middleware(['auth:sanctum'])->prefix('common')->group(function ($router) {
+    $router->post('generate-title-image', [CommonController::class, 'generateTitleImage']);
+});
+
